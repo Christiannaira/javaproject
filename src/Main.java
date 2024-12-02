4bee7dae094348182961c1c40c53920136036f52
Christian <Christianmnaira@gmail.com>
2024-12-02 09:10:04 +0800

    feat: multi-line comment

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // This is a comment
-        System.out.println("Hello World");
-
-        System.out.println("Hello World"); //This is a comment
+        /* The code below will print the word Hello World
+         to the screen and it is amazing */
+        System.out.println("hello world");
 
     }
 
