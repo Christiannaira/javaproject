22fa0ae346077923323dff50624c8e40a4adfc5e
Christian <Christianmnaira@gmail.com>
2024-12-02 09:08:49 +0800

    feat: comments

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(3 + 3);
-        System.out.println(2 * 5);
-        System.out.println(5 - 2);
-        System.out.println(10 / 2);
-        System.out.println(10 % 3);
+        // This is a comment
+        System.out.println("Hello World");
+
+        System.out.println("Hello World"); //This is a comment
 
     }
 
