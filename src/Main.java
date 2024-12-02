858fb16e97c166053e37401e32e74c24c9d80f99
Christian <Christianmnaira@gmail.com>
2024-12-02 09:00:14 +0800

    feat: println samples

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("hello world");
+        System.out.println("Hello World!");
+        System.out.println("I am learning Java.");
+        System.out.println("It is awesome!");
 
     }
 
