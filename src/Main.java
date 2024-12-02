99f3b33cb452b104f95cd90efa5c5da783dcfdee
Christian <Christianmnaira@gmail.com>
2024-12-02 09:13:27 +0800

    feat: intro to variables

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /* The code below will print the word Hello World
-         to the screen and it is amazing */
-        System.out.println("hello world");
+        String name = "John";
+        System.out.println(name);
+
+        int myNum = 15;
+        System.out.println(myNum);
+
+        int myNum2 = 10;
+        myNum2 = 20;
+        System.out.println(myNum2);
+
+        int myNum3;
+        myNum3 = 55;
+        System.out.println(myNum3);
 
     }
 
