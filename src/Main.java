c1be1fd8278df1b183ec6d23c05a13570598901e
Christian <Christianmnaira@gmail.com>
2024-12-02 09:15:17 +0800

    feat: final variable

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String name = "John";
-        System.out.println(name);
+        final int myNum = 15;
 
-        int myNum = 15;
+
         System.out.println(myNum);
-
-        int myNum2 = 10;
-        myNum2 = 20;
-        System.out.println(myNum2);
-
-        int myNum3;
-        myNum3 = 55;
-        System.out.println(myNum3);
 
     }
 
