9b7e5f4ae49d92da735102064f7648e0ba03ce3d
Christian <Christianmnaira@gmail.com>
2024-12-03 13:58:49 +0800

    feat: logical operators

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-      int sum1 = 100 + 50;
-      int sum2 = sum1 + 250;
-      int sum3 = sum2 + sum2;
-
-      System.out.println(sum1);
-      System.out.println(sum2);
-      System.out.println(sum3);
+        int x = 5;
+        System.out.println(x > 3 && x < 10);
+        System.out.println(x > 3 || x < 4);
+        System.out.println(!(x > 3 && x < 10));
 
     }
 
