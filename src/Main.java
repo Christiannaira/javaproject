92ad89189be285ec86399f73a3d3306f400488f0
Christian <Christianmnaira@gmail.com>
2024-12-03 13:45:59 +0800

    feat: widening casting

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-       String studentName = "John Doe";
-       int studentID = 15;
-       int studentAge = 23;
-       float studentFee = 75.25f;
-       char studentGrade = 'B';
+        int myInt = 9;
+        double myDouble = myInt;
 
-        System.out.println("Student name: " + studentName);
-        System.out.println("Student id: " + studentID);
-        System.out.println("Student age: " + studentAge);
-        System.out.println("Student fee: " + studentFee);
-        System.out.println("Student grade: " + studentGrade);
+        byte myByte = 2;
+        float myFloat = myByte;
+
+        System.out.println(myByte);
+        System.out.println(myFloat);
 
     }
 
