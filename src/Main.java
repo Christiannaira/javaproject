7d2d2bea71f20713ddc761dbd409340083f5afac
Christian <Christianmnaira@gmail.com>
2024-12-03 13:55:18 +0800

    feat: arithmetic operators

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int maxScore = 500;
+      int sum1 = 100 + 50;
+      int sum2 = sum1 + 250;
+      int sum3 = sum2 + sum2;
 
-        int userScore = 423;
-
-        float percentage = (float) userScore / maxScore * 100.0f;
-
-        System.out.println("User's percentage is " + percentage);
+      System.out.println(sum1);
+      System.out.println(sum2);
+      System.out.println(sum3);
 
     }
 
