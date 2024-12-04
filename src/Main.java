1175cf6764ee64958c11f2504940e782e5a2609b
Christian <Christianmnaira@gmail.com>
2024-12-05 07:51:48 +0800

    feat: for loop

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int dice = 1;
-
-        while (dice <= 6){
-
-            if (dice < 6) {
-
-                System.out.println("No Yatzy.");
-
-            } else {
-
-                System.out.println("Yatzy!");
-
-            }
-
-            dice = dice + 1;
-
-        }
+      for (int i = 0; i < 5; i++) {
+          System.out.println(i);
+      }
 
     }
 
