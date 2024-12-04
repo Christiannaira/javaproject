8207c89f524c2987cb2c0e246ba156ef716a5375
Christian <Christianmnaira@gmail.com>
2024-12-04 21:05:39 +0800

    feat: while loop

--- src/Main.java
+++ src/Main.java
@@ -3,32 +3,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int day = 10;
+       int countdown = 3;
 
-        switch (day) {
+       while (countdown > 0) {
+           System.out.println(countdown);
+           countdown--;
+       }
 
-            case 1:
-                System.out.println("Monday");
-                break;
-            case 2:
-                System.out.println("Tuesday");
-                break;
-            case 3:
-                System.out.println("Wednesday");
-                break;
-            case 4:
-                System.out.println("Thursday");
-                break;
-            case 5:
-                System.out.println("Friday");
-                break;
-            case 6:
-                System.out.println("Saturday");
-                break;
-            default:
-                System.out.println("none");
-
-        }
+        System.out.println("Happy New Year!!");
 
     }
 
