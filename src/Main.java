0a6d3d692a8b3d40ea25fed019c201b20624417a
Christian <Christianmnaira@gmail.com>
2024-12-04 20:55:06 +0800

    feat: short hand | ternary operator

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int time = 22;
+      int time = 20;
 
-        if (time < 10) {
-            System.out.println("Good morning.");
-        } else if (time < 18) {
-            System.out.println("Good day.");
-        } else {
-            System.out.println("Good evening");
-        }
+      String result;
+      result = (time < 18)? "Good day." : "Good evening.";
+      System.out.println(result);
 
     }
 
