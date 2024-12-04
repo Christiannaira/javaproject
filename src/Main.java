3dcf7679fa6f98b00ac1451dfc46243df15c2ca1
Christian <Christianmnaira@gmail.com>
2024-12-04 20:51:21 +0800

    feat: if else statements

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String firstName = "John ";
-        String lastName = "Doe";
-        System.out.println(firstName.concat(lastName));
+      if (20 > 18) {
+          System.out.println("20 is greater than 18");
+      }
+
+      int x = 20;
+      int y = 18;
+
+      if (x > y) {
+          System.out.println("x is greater than y");
+      }
 
     }
 
