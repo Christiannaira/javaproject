21292571b84ede1aaa0070c2f333b24cfa0578c3
Christian <Christianmnaira@gmail.com>
2024-12-04 21:07:01 +0800

    feat: while loop

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,23 @@ public class Main {
 
     public static void main(String[] args) {
 
-       int countdown = 3;
+        int dice = 1;
 
-       while (countdown > 0) {
-           System.out.println(countdown);
-           countdown--;
-       }
+        while (dice <= 6){
 
-        System.out.println("Happy New Year!!");
+            if (dice < 6) {
+
+                System.out.println("No Yatzy.");
+
+            } else {
+
+                System.out.println("Yatzy!");
+
+            }
+
+            dice = dice + 1;
+
+        }
 
     }
 
