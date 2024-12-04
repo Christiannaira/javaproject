f52122cab22855dd3a14d780187698da3a344f26
Christian <Christianmnaira@gmail.com>
2024-12-05 07:58:32 +0800

    feat: break

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-      int number = 2;
+        for (int i = 0; i < 10; i++) {
 
-      for (int i = 1; i <= 10; i++) {
+            if (i == 4) {
+                break;
+            }
+            System.out.println(i);
 
-          System.out.println(number + " x " + i + " = " + (number * i));
-
-      }
-
+        }
 
     }
 
