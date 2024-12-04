8f8fbb50a416f2df0a41145dbd7f5773f725798b
Christian <Christianmnaira@gmail.com>
2024-12-05 07:54:00 +0800

    feat: nested loops

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-      for (int i = 0; i < 5; i++) {
-          System.out.println(i);
-      }
+     for (int i = 1; i <= 2; i++) {
+         System.out.println("Outer: " + i);
 
-      for (int i = 0; i <= 10; i = i + 2) {
-          System.out.println(i);
-      }
+         for (int j = 1; j <= 3; j++) {
+
+             System.out.println(" Inner: " + j);
+
+         }
+
+     }
 
     }
 
