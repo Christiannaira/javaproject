ff3b21c3db0b722fe3280786087714f01bfd0813
Christian <Christianmnaira@gmail.com>
2024-12-05 07:55:13 +0800

    feat: for each

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-     for (int i = 1; i <= 2; i++) {
-         System.out.println("Outer: " + i);
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
 
-         for (int j = 1; j <= 3; j++) {
+        for (String i : cars) {
 
-             System.out.println(" Inner: " + j);
+            System.out.println(i);
 
-         }
-
-     }
+        }
 
     }
 
