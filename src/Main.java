8c39782cebcbe90fcacaeace91a2ed11fda7875e
Christian <Christianmnaira@gmail.com>
2024-12-06 20:58:21 +0800

    feat: scope

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,22 @@
 
 public class Main {
 
-    static int plusMethod(int x, int y) {
 
-        return x + y;
-
-    }
-
-     static double plusMethod(double x, double y) {
-
-        return x + y;
-
-     }
 
     public static void main(String[] args) {
 
-        int myNum1 = plusMethod(8, 5);
-        double myNum2 = plusMethod(4.3, 6.26);
+        int x = 100;
 
-        System.out.println("int: " + myNum1);
-        System.out.println("double: " + myNum2);
+        System.out.println(x);
+
+        {
+
+            int y = 100;
+
+            System.out.println(y);
+
+        }
+
 
 
     }
