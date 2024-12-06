016a3702f82a62ffda87b1c6e21d23ce7d6963be
Christian <Christianmnaira@gmail.com>
2024-12-06 20:39:39 +0800

    feat: multiple parameters

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 
 public class Main {
 
-    static void myMethod(String fname) {
+    static void myMethod(String fname, int age) {
 
-        System.out.println(fname + " Refsnes");
+        System.out.println(fname + " is " + age);
 
     }
 
     public static void main(String[] args) {
 
-        myMethod("Liam");
-        myMethod("Jenny");
-        myMethod("Anja");
+        myMethod("Liam", 5);
+        myMethod("Jenny", 8);
+        myMethod("Anja", 31);
 
     }
 
