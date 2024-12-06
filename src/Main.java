a2db24ce89d6a0f8335a6a88275002d4cda799f3
Christian <Christianmnaira@gmail.com>
2024-12-06 20:56:29 +0800

    feat: method overloading

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,26 @@
 
 public class Main {
 
-    static int plusMethodInt(int x, int y) {
+    static int plusMethod(int x, int y) {
 
         return x + y;
 
     }
 
-    static double plusMethodDouble(double x, double y) {
+     static double plusMethod(double x, double y) {
+
         return x + y;
-    }
+
+     }
 
     public static void main(String[] args) {
 
-        int myNum1 = plusMethodInt(8, 5);
-        double myNum2 = plusMethodDouble(4.3, 6.26);
+        int myNum1 = plusMethod(8, 5);
+        double myNum2 = plusMethod(4.3, 6.26);
+
         System.out.println("int: " + myNum1);
         System.out.println("double: " + myNum2);
+
 
     }
 
