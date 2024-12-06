552cda57146f1aa06d47b01eaf5046aadfa7af80
Christian <Christianmnaira@gmail.com>
2024-12-06 20:34:42 +0800

    feat: method

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,17 @@
 
 public class Main {
 
+    static void myMethod() {
+
+        System.out.println("I just got executed!");
+
+    }
+
     public static void main(String[] args) {
 
-       int[][] myNumbers = {{1, 2, 3, 4}, {5, 6, 7}};
-
-       for (int[] row : myNumbers) {
-
-           for (int i : row) {
-
-               System.out.println(i);
-
-           }
-
-       }
+      myMethod();
+      myMethod();
+      myMethod();
 
     }
 
