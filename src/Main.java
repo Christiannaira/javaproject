74681273a223a46a09deebc4cc095eee9aa61788
Christian <Christianmnaira@gmail.com>
2024-12-08 21:56:54 +0800

    feat: count number of words in a string

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int x, y, sum;
-        Scanner myObj = new Scanner(System.in);
-        System.out.println("Type a number:");
-        x = myObj.nextInt();
-
-        System.out.println("Type another number:");
-        y = myObj.nextInt();
-
-        sum = x + y;
-        System.out.println("Sum is: " + sum);
+       String words = "One Two Three Four";
+       int countWords = words.split("\\s").length;
+        System.out.println(countWords);
 
     }
 
