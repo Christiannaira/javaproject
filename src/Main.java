823e1c61d4eb605cf14e3de7ed6f13ab48d8b0e6
Christian <Christianmnaira@gmail.com>
2024-12-08 21:53:44 +0800

    feat: add two numbers

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,13 @@
 
 public class Main {
 
-    public static int sum(int start, int end) {
-
-        if (end > start) {
-
-            return end + sum(start, end - 1);
-
-        } else {
-
-            return end;
-
-        }
-
-    }
-
     public static void main(String[] args) {
 
-       int result = sum(5, 10);
-        System.out.println(result);
+        int x = 5;
+        int y = 6;
+        int sum = x + y;
+
+        System.out.println(sum);
 
     }
 
