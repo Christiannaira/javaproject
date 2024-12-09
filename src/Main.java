6d3638747aca6f9d6a6ed83294358c39265c570b
Christian <Christianmnaira@gmail.com>
2024-12-09 09:04:05 +0800

    feat: Math random

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(Math.random());
+        int randomNum = (int)(Math.random() * 101);
+        System.out.println(randomNum);
 
     }
 
