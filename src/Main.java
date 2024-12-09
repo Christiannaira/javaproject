53bd2003b48145d54b16cc0d96385d6cca4ecefe
Christian <Christianmnaira@gmail.com>
2024-12-09 08:56:31 +0800

    feat: Hashmap values loop

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
         capitalCities.put("Norway", "Oslo");
         capitalCities.put("USA", "Washington DC");
 
-        for (String i: capitalCities.keySet()) {
+        for (String i: capitalCities.values()) {
 
             System.out.println(i);
 
