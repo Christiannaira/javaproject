a563d62d47e638bef094320b1c683de6189bf57a
Christian <Christianmnaira@gmail.com>
2024-12-09 09:49:24 +0800

    feat: arraylist sorting integers

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-       ArrayList<String> cars = new ArrayList<String>();
-       cars.add("Volvo");
-       cars.add("BMW");
-       cars.add("Ford");
-       cars.add("Mazda");
+        ArrayList<Integer> myNumbers = new ArrayList<Integer>();
 
-        Collections.sort(cars);
+        myNumbers.add(33);
+        myNumbers.add(15);
+        myNumbers.add(20);
+        myNumbers.add(34);
+        myNumbers.add(8);
+        myNumbers.add(12);
 
-        for (String i : cars) {
+        Collections.sort(myNumbers);
+
+        for (int i : myNumbers) {
             System.out.println(i);
         }
 
