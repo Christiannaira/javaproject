dd5b3b7e0035d0151a43c2d7ddabb2d98b4d2722
Christian <Christianmnaira@gmail.com>
2024-12-09 09:21:09 +0800

    feat: built-in array loop

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,9 @@ public class Main {
 
         int arrayLength = numbers.length;
 
-        System.out.println(arrayLength);
-
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
 
 
     }
