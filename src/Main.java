e97a1ea63564f2cb00dd2882fe5808cb94fccd3a
Christian <Christianmnaira@gmail.com>
2024-12-09 08:41:24 +0800

    feat: convert string to array

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,11 @@
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int[] myArray = {1, 5, 10, 25};
-        int sum = 0;
-        int i;
-
-        for (i = 0; i < myArray.length; i++) {
-            sum += myArray[i];
-        }
-
-        System.out.println("The sum is: " + sum);
+        String myStr = "Hello";
+        char[] myArray = myStr.toCharArray();
+        System.out.println(myArray[0]);
 
     }
 
