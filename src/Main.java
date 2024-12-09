2af0745eb128632b33b2e643d418de2f630c5d5c
Christian <Christianmnaira@gmail.com>
2024-12-09 09:02:44 +0800

    feat: square root of a number

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int myNum = -10;
-
-        if (myNum > 0) {
-            System.out.println("The value is a positive number.");
-        } else if (myNum < 0) {
-            System.out.println("The value is a negative number");
-        } else {
-            System.out.println("The value is 0.");
-        }
+        System.out.println(Math.sqrt(64));
 
     }
 
