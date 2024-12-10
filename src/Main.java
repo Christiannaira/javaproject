3654139f169ae6d2003f2146a635405f3d7f017d
Christian <Christianmnaira@gmail.com>
2024-12-10 12:56:59 +0800

    feat: error handling

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ArrayList<ArrayList<String>> userAccounts = new ArrayList<>();
-
-        ArrayList<String> user1 = new ArrayList<>();
-        user1.add("Christian Naira");
-        user1.add("1234");
-        user1.add("5000");
-
-        ArrayList<String> user2 = new ArrayList<>();
-        user2.add("John Doe");
-        user2.add("4321");
-        user2.add("10000");
-
-        System.out.println(userAccounts);
-
-        userAccounts.add(user1);
-        userAccounts.add(user2);
-
-        System.out.println(userAccounts);
+       try {
+           int[] myNumbers = {1, 2, 3};
+           System.out.println(myNumbers[10]);
+       } catch (Exception e) {
+           System.out.println("Something went wrong.");
+       }
 
 
     }
