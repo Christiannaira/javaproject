4be12927716a220dcf9185595c8020fdf526ec9f
Christian <Christianmnaira@gmail.com>
2024-12-10 12:59:19 +0800

    feat: throwing an error

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,19 @@ import java.util.ArrayList;
 
 public class Main {
 
+    static void checkAge(int age) {
+
+        if (age < 18) {
+            throw new ArithmeticException("Access denied - You must be at least 18 years old.");
+        } else {
+            System.out.println("Access granted - You are old enough!");
+        }
+
+    }
+
     public static void main(String[] args) {
 
-       try {
-           int[] myNumbers = {1, 2, 3};
-           System.out.println(myNumbers[10]);
-       } catch (Exception e) {
-           System.out.println("Something went wrong.");
-       } finally {
-           System.out.println("finally");
-       }
-
+        checkAge(15);
 
     }
 
