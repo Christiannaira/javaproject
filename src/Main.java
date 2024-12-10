4c0e5fc9f718b47560e57892a0653b5f0dda2535
Christian <Christianmnaira@gmail.com>
2024-12-10 12:54:49 +0800

    feat: Multi-Dimensional ArrayList String

--- src/Main.java
+++ src/Main.java
@@ -4,40 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ArrayList<ArrayList<Integer>> multiDimList = new ArrayList<>();
+        ArrayList<ArrayList<String>> userAccounts = new ArrayList<>();
 
-        ArrayList<Integer> row1 = new ArrayList<>();
-        row1.add(1);
-        row1.add(2);
-        row1.add(3);
+        ArrayList<String> user1 = new ArrayList<>();
+        user1.add("Christian Naira");
+        user1.add("1234");
+        user1.add("5000");
 
-        ArrayList<Integer> row2 = new ArrayList<>();
-        row2.add(4);
-        row2.add(5);
-        row2.add(6);
+        ArrayList<String> user2 = new ArrayList<>();
+        user2.add("John Doe");
+        user2.add("4321");
+        user2.add("10000");
 
-        multiDimList.add(row1);
-        multiDimList.add(row2);
+        System.out.println(userAccounts);
 
-        ArrayList<Integer> row3 = new ArrayList<>();
-        row3.add(7);
-        row3.add(8);
-        row3.add(9);
+        userAccounts.add(user1);
+        userAccounts.add(user2);
 
-        multiDimList.add(row3);
-
-        System.out.println(multiDimList);
-        System.out.println(multiDimList.get(0).get(1));
-
-        for (int i = 0; i < multiDimList.size(); i++) {
-
-            for (int j = 0; j < multiDimList.get(i).size(); j++) {
-
-                System.out.println(multiDimList.get(i).get(j));
-
-            }
-
-        }
+        System.out.println(userAccounts);
 
 
     }
