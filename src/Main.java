fac45f0fe7744371ea8b3f55ccc88b2672782f73
Christian <Christianmnaira@gmail.com>
2024-12-10 10:29:25 +0800

    feat: ArrayList code

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,27 @@ public class Main {
         System.out.println(cars.get(2));
         System.out.println(cars.get(3));
 
+        cars.add(0, "First");
+        System.out.println(cars);
+
+        cars.set(0, "Opel");
+        System.out.println(cars);
+
+        cars.remove(0);
+        System.out.println(cars);
+
+//        cars.clear();
+
+        System.out.println(cars.size());
+
+        for (int i = 0; i < cars.size(); i++) {
+            System.out.println(cars.get(i));
+        }
+
+        for (String i : cars) {
+            System.out.println(i);
+        }
+
 
     }
 
