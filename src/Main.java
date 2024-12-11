3c060905376f0f42698df78cbea3a24a5287bb3d
Christian <Christianmnaira@gmail.com>
2024-12-11 15:52:33 +0800

    feat: delete a folder

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        File myObj = new File("filename2.txt");
-
+        File myObj = new File("C:\\Users\\USER\\IdeaProjects\\javaproject\\Test");
         if (myObj.delete()) {
-            System.out.println("Deleted the file: " + myObj.getName());
+            System.out.println("Deleted the folder: " + myObj.getName());
         } else {
-            System.out.println("Failed to delete the file.");
+            System.out.println("Failed to delete the folder");
         }
 
     }
