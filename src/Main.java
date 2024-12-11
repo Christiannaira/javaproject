cdd9765bfb08eb3450d1319f039aee2d8ba085d5
Christian <Christianmnaira@gmail.com>
2024-12-11 15:44:18 +0800

    feat: wrote to the file 2

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,10 @@ public class Main {
     public static void main(String[] args) {
 
        try {
-           FileWriter myWriter = new FileWriter("filename.txt");
-           myWriter.write("Files in Java might be tricky, but it is fun enough!");
-           myWriter.close();
+           FileWriter writing = new FileWriter("filename2.txt");
+           writing.write("Hello Universe");
+           writing.close();
+
            System.out.println("Successfully wrote to the file.");
 
        } catch (IOException e) {
