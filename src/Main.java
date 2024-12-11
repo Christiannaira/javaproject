2ec1d75d11cb6f54075e9cb2522077c8a71b87bd
Christian <Christianmnaira@gmail.com>
2024-12-11 15:49:31 +0800

    feat: reading file information

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,19 @@
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        try {
+        File myObj = new File("filename.txt");
 
-            File myObj = new File("filename.txt");
-            Scanner myReader = new Scanner(myObj);
-
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                System.out.println(data);
-            }
-
-        } catch (FileNotFoundException e) {
-
-            System.out.println("An error occured");
-            e.printStackTrace();
-
+        if (myObj.exists()) {
+            System.out.println("File name: " + myObj.getName());
+            System.out.println("Absolute path: " + myObj.getAbsolutePath());
+            System.out.println("Writeable: " + myObj.canWrite());
+            System.out.println("Readable: " + myObj.canRead());
+            System.out.println("File size in bytes: " + myObj.length());
+        } else {
+            System.out.println("The file does not exist");
         }
 
     }
