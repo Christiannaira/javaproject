56ecd972281efbb418fd5f0a9664428e39b904fb
Christian <Christianmnaira@gmail.com>
2024-12-11 15:42:38 +0800

    feat: wrote to the file

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,21 @@
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        try {
-            File myObj = new File("C:\\Users\\USER\\IdeaProjects\\javaproject\\filename2.txt");
+       try {
+           FileWriter myWriter = new FileWriter("filename.txt");
+           myWriter.write("Files in Java might be tricky, but it is fun enough!");
+           myWriter.close();
+           System.out.println("Successfully wrote to the file.");
 
-            if (myObj.createNewFile()) {
-                System.out.println("File created: " + myObj.getName());
-                System.out.println("Absolute path: " + myObj.getAbsolutePath());
-            } else {
-                System.out.println("File already exists.");
-            }
-
-        } catch (IOException e) {
-            System.out.println("An error occured.");
-            e.printStackTrace();
-        }
+       } catch (IOException e) {
+           System.out.println("An error occurred");
+           e.printStackTrace();
+       }
 
     }
 
