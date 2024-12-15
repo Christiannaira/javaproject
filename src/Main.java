32739c926c3abb416139ff709135b9c81db48655
Christian <Christianmnaira@gmail.com>
2024-12-15 19:59:05 +0800

    feat: modifying class attributes

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
-import java.io.File;
-import java.util.Scanner;
+
 
 public class Main {
 
-
+    int x;
 
     public static void main(String[] args) {
 
-       Second myObj = new Second();
+        Main myObj = new Main();
+        myObj.x = 40;
         System.out.println(myObj.x);
 
     }
