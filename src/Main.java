4662de698e754f31973e87157add8a31e7bc7c11
Christian <Christianmnaira@gmail.com>
2024-12-15 20:18:53 +0800

    feat: class methods usage

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,19 @@
 
 public class Main {
 
-    static void myStaticMethod() {
-        System.out.println("static");
+    public void fullThrottle() {
+        System.out.println("The car is going as fast as it can!");
     }
 
-    public void publicMethod() {
-        System.out.println("public");
+    public void speed(int maxSpeed) {
+        System.out.println("Max speed is: " + maxSpeed);
     }
 
     public static void main(String[] args) {
 
-        myStaticMethod();
-
-        Main myObj = new Main();
-        myObj.publicMethod();
+       Main myCar = new Main();
+       myCar.fullThrottle();
+       myCar.speed(200);
 
     }
 
