64f5a668ba072b58311becb9081775e40f32eaa1
Christian <Christianmnaira@gmail.com>
2024-12-15 19:54:24 +0800

    feat: creating multiple class objects

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Main myObj = new Main();
-        System.out.println(myObj.x);
+        Main myObj1 = new Main();
+        Main myObj2 = new Main();
+
+        System.out.println(myObj1.x);
+        System.out.println(myObj2.x);
 
     }
 
