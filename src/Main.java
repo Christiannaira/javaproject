c12213fb820540866ea8174a0cded9f1b1a79b9d
Christian <Christianmnaira@gmail.com>
2024-12-15 19:56:12 +0800

    feat: created a second class

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,12 @@ import java.util.Scanner;
 
 public class Main {
 
-    int x = 5;
+
 
     public static void main(String[] args) {
 
-        Main myObj1 = new Main();
-        Main myObj2 = new Main();
-
-        System.out.println(myObj1.x);
-        System.out.println(myObj2.x);
+       Second myObj = new Second();
+        System.out.println(myObj.x);
 
     }
 
