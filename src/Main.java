f87c014933c70783e6b9d8e666fa3a58093a25d5
Christian <Christianmnaira@gmail.com>
2024-12-15 20:00:01 +0800

    feat: class final attributes

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,11 @@
 
 public class Main {
 
-    int x = 10;
+    final int x = 10;
 
     public static void main(String[] args) {
 
         Main myObj = new Main();
-        myObj.x = 40;
         System.out.println(myObj.x);
 
     }
