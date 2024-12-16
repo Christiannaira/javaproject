8c236ad84b703c38d3ab62225416adc8e09f422b
Christian <Christianmnaira@gmail.com>
2024-12-16 19:47:04 +0800

    fix: java constructors

--- src/Main.java
+++ src/Main.java
@@ -2,55 +2,16 @@ import java.util.Scanner;
 
 public class Main {
 
+    int x;
+
+    public Main() {
+        x = 5;
+    }
+
     public static void main(String[] args) {
 
-      Scanner askuser = new Scanner(System.in);
-
-      int i = 0;
-      while (i < 5) {
-
-          System.out.println("Enter username: ");
-          String username = askuser.nextLine();
-
-          if (username.equals("Christian")) {
-
-              int j = 1;
-              while (j == 1) {
-
-                  Scanner askUserNumber = new Scanner(System.in);
-                  System.out.println("Enter your choice number (1, 2, 3, 4(exit)): ");
-                  int userNumber = askUserNumber.nextInt();
-
-                  if (userNumber == 1) {
-
-                      System.out.println("1");
-
-                  } else if (userNumber == 2) {
-
-                      System.out.println("2");
-
-                  } else if (userNumber == 3) {
-
-                      System.out.println("3");
-
-                  } else if (userNumber == 4) {
-
-                      System.out.println("logging out...");
-                      j = 0;
-
-
-                  }
-
-
-
-              }
-
-          }
-
-          i++;
-
-
-      }
+        Main myObj = new Main();
+        System.out.println(myObj.x);
 
     }
 
