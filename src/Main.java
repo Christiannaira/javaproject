90b4dae4dbd7f6e67eb3f07427675986d5451e29
Christian <Christianmnaira@gmail.com>
2024-12-16 19:49:43 +0800

    fix: class multiple parameters

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,18 @@ import java.util.Scanner;
 
 public class Main {
 
-    int x;
+    int modelYear;
+    String modelName;
 
-    public Main(int y) {
-        x = y;
+    public Main(int year, String name) {
+        modelYear = year;
+        modelName = name;
     }
 
     public static void main(String[] args) {
 
-        Main myObj = new Main(5);
-        System.out.println(myObj.x);
+        Main myCar = new Main(1969, "Mustang");
+        System.out.println(myCar.modelYear + " " + myCar.modelName);
 
     }
 
