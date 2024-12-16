965629ef71d63344782619f3dc6c3f4f773b31f0
Christian <Christianmnaira@gmail.com>
2024-12-16 19:54:01 +0800

    fix: static and public modifier

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,20 @@ import java.util.Scanner;
 
 public class Main {
 
-    final int x = 10;
-    final double PI = 3.14;
+    static void myStaticMethod() {
+        System.out.println("static");
+    }
+
+    public void myPublicMethod() {
+        System.out.println("public");
+    }
 
     public static void main(String[] args) {
 
+        myStaticMethod();
 
+        Main myObj = new Main();
+        myObj.myPublicMethod();
 
     }
 
