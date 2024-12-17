74530f010793bba28a26a7d0c58af4adc04912be
Christian <Christianmnaira@gmail.com>
2024-12-17 17:50:10 +0800

    feat: string constructors

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,10 @@ public class Main {
 
     public static void main(String[] args){
 
-        String str = "Hello";
-        String str2 = new String("Hello World");
+      char[] charArray = {'H', 'e', 'l', 'l', 'o'};
+      String str = new String(charArray);
 
-        System.out.println(str);
-        System.out.println(str2);
+      System.out.println(str);
 
 
     }
