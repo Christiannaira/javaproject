9923d91241709da6ad195321556100a61c391091
Christian <Christianmnaira@gmail.com>
2024-12-17 17:53:32 +0800

    feat: string constructors

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args){
 
       char[] charArray = {'p', 'e', 'a', 'c', 'e', 'f', 'u', 'l'};
-      String str = new String(charArray, 2, 3);
+      String str = new String(charArray, 0, 5);
 
       System.out.println(str);
 
