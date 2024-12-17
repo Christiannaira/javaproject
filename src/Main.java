07838f9bfa7b2a2fb42d570700e146d8a2735739
Christian <Christianmnaira@gmail.com>
2024-12-17 17:27:23 +0800

    feat: directly assigning a string literal

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,10 @@ public class Main {
     public static void main(String[] args){
 
         String greeting = "Hello World!";
+        String strName;
+        strName = "Harley";
+
+        System.out.println(strName);
 
     }
 
