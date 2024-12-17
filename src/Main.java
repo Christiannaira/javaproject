783ea1683fe3f46e262fba728e21598e19712d7d
Christian <Christianmnaira@gmail.com>
2024-12-17 18:01:26 +0800

    feat: string constructors

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,9 @@ public class Main {
 
     public static void main(String[] args){
 
-      byte b[] = {70, 114, 101, 101, 100, 111, 109};
-      String s = new String(b);
+      byte b[] = {66, 82, 65, 86, 69, 82, 89};
+      String s = new String(b, 1, 4);
       System.out.println(s);
-
 
     }
 
