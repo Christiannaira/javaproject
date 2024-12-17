30959447eb16926d857491704300152f03703d0a
Christian <Christianmnaira@gmail.com>
2024-12-17 18:10:32 +0800

    feat: string method toLowerCase

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,9 @@ public class Main {
 
     public static void main(String[] args){
 
-      String strA = "Learn Java";
-      String strB = "Learn Java";
-      String strC = "Learn Kolin";
-      boolean result;
+      String strA = "BREATHE";
 
-      result = strA.equals(strB);
-      System.out.println(result);
-
-      result = strA.equals(strC);
-      System.out.println(result);
-
-      result = strC.equals(strA);
-      System.out.println(result);
+      System.out.println(strA.toLowerCase());
 
 
     }
