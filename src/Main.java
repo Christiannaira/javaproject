88fe2a17150d5d1ac2df655249b6765afb32d9c3
Christian <Christianmnaira@gmail.com>
2024-12-19 10:31:02 +0800

    feat: String.toString()

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        char[] myArray = {'a', 'b', 'c'};
+        String myStr = "Hello, World!";
 
-        System.out.println(String.valueOf(myArray));
-        System.out.println(String.valueOf('A'));
-        System.out.println(String.valueOf(true));
-        System.out.println(String.valueOf(4.5f));
-        System.out.println(String.valueOf(5.2));
-        System.out.println(String.valueOf(12));
-        System.out.println(String.valueOf(1400L));
+        System.out.println(myStr.toString());
 
     }
 
