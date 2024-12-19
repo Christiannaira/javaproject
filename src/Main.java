306b988c5908a74794ed2b0f46f87952b5e53b04
Christian <Christianmnaira@gmail.com>
2024-12-19 10:48:01 +0800

    feat: replaceAll()

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String myStr = "Hello";
-        System.out.println(myStr.replace('l', 'p'));
+        String myStr = "I love cats. Cars are very easy to love. Cats are very popular.";
+        String regex = "(?i)cat";
+        System.out.println(myStr.replaceAll(regex, "dog"));
 
     }
 
