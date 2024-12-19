df390d92885b39e66d80af8e744acedd5a3e8834
Christian <Christianmnaira@gmail.com>
2024-12-19 10:49:40 +0800

    feat: replaceFirst()

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String myStr = "I love cats. Cars are very easy to love. Cats are very popular.";
-        String regex = "(?i)cat";
-        System.out.println(myStr.replaceAll(regex, "dog"));
+        String myStr = "This is W3Schools";
+        String regex = "is";
+        System.out.println(myStr.replaceFirst(regex, "at"));
 
     }
 
