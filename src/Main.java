12ac1c690bc6a5aa456f246a13c87b5f37ad07b1
Christian <Christianmnaira@gmail.com>
2024-12-19 10:28:15 +0800

    feat: String.indexOf()

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         String myStr = "Hello planet earth, you are a great planet";
-        System.out.println(myStr.indexOf("planet"));
+        System.out.println(myStr.indexOf("e", 5));
 
     }
 
