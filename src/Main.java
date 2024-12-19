5c82f55bc3d3864771c1bae6bc827870234c87ad
Christian <Christianmnaira@gmail.com>
2024-12-19 10:33:25 +0800

    feat: toLowerCase() toUpperCase()

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String myStr = "    Hello World     ";
-        System.out.println(myStr);
-        System.out.println(myStr.trim());
+        String myStr = "Hello World";
+        System.out.println(myStr.toUpperCase());
+        System.out.println(myStr.toLowerCase());
 
     }
 
