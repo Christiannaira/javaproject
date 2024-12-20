24c2470a6667027c064b40c093ce2bd5775e13f2
Christian <Christianmnaira@gmail.com>
2024-12-20 22:15:55 +0800

    feat: removeIf()

--- src/Main.java
+++ src/Main.java
@@ -6,21 +6,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ArrayList<String> cars = new ArrayList<String>();
-        cars.add("Volvo");
-        cars.add("BMW");
-        cars.add("Ford");
-        cars.add("Mazda");
-        cars.add("Toyota");
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(5);
+        numbers.add(9);
+        numbers.add(8);
+        numbers.add(6);
+        numbers.add(1);
+        numbers.removeIf(n -> n % 2 == 0);
+        System.out.println(numbers);
 
-        ArrayList<String> remove = new ArrayList<String>();
-        remove.add("Volvo");
-        remove.add("Ford");
-        remove.add("Mazda");
-
-        cars.removeAll(remove);
-
-        System.out.println(cars);
 
 
     }
