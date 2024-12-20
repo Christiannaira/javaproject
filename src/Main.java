cd6d2efa999bd47e5f3c126d0fbef81aa9c2a0d9
Christian <Christianmnaira@gmail.com>
2024-12-20 22:11:36 +0800

    feat: remove();

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.ListIterator;
 
 
 public class Main {
@@ -12,18 +11,10 @@ public class Main {
         cars.add("Ford");
         cars.add("Mazda");
 
-        ListIterator<String> it = cars.listIterator();
 
-        while (it.hasNext()) {
-            System.out.println(it.next());
-        }
-
-        System.out.println("---");
-
-        while (it.hasPrevious()) {
-            System.out.println(it.previous());
-        }
-
+        cars.remove("Ford");
+        cars.remove(0);
+        System.out.println(cars);
 
     }
 
