9bcc501bbb2e55aa89c67ea564bd5c34bfd968e6
Christian <Christianmnaira@gmail.com>
2024-12-20 22:04:52 +0800

    feat: ArrayList.get()

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ArrayList<Integer> numbers = new ArrayList<Integer>();
-
-        numbers.add(5);
-        numbers.add(9);
-        numbers.add(8);
-        numbers.add(1);
-
-        numbers.forEach((n) -> {
-            System.out.println(n);
-        });
-
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        System.out.println(cars.get(0));
 
     }
 
