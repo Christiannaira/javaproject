0513caf4dcfefc47dcb8f98cbc8e8786b125b02d
Christian <Christianmnaira@gmail.com>
2024-12-22 19:41:08 +0800

    feat: hashMap.merge()

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,11 @@ public class Main {
         capitalCities.put("Norway", "Oslo");
         capitalCities.put("USA", "Washington DC");
 
-        System.out.println(capitalCities.keySet());
+        capitalCities.merge("Canada", "Ottawa", (a, b) -> a + " -> " + b);
+        capitalCities.merge("England", "London", (a, b) -> a + " -> " + b);
+        capitalCities.merge("Germany", "Berlin", (a, b) -> a + " -> " + b);
+
+        System.out.println(capitalCities);
 
 
     }
