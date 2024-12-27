0889cf00f0c008b28abd20dca24c251a15c5d10a
Christian <Christianmnaira@gmail.com>
2024-12-27 19:16:09 +0800

    feat: tried to output the cars ArrayList

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,15 @@ public class Main {
         cars.add("Mazda");
 
         System.out.println(cars);
+        System.out.println(" ");
+        for (String car : cars) {
+            System.out.println(car);
+        }
 
+        System.out.println(" ");
+        for (int i = 0; i < cars.size(); i++) {
+            System.out.println(cars.get(i));
+        }
 
     }
 
