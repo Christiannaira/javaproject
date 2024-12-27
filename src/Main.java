bb52849f0f0735376d3d60518e0e304d9f5c977f
Christian <Christianmnaira@gmail.com>
2024-12-27 19:22:54 +0800

    feat: sorting the myNumbers ArrayList

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
 
@@ -18,6 +19,8 @@ public class Main {
         myNumbers.add(15);
         myNumbers.add(20);
         myNumbers.add(25);
+
+        Collections.sort(myNumbers);
 
         for (int i : myNumbers) {
             System.out.println(i);
