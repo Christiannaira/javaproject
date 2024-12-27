5af237e5a0cdadb53c0f2b2092ff7397e7a17467
Christian <Christianmnaira@gmail.com>
2024-12-27 19:17:32 +0800

    feat: added an items in cars ArrayList with a specified position index

--- src/Main.java
+++ src/Main.java
@@ -13,16 +13,15 @@ public class Main {
         cars.add("Ford");
         cars.add("Mazda");
 
+        cars.add(0, "Mazda");
+
         System.out.println(cars);
         System.out.println(" ");
         for (String car : cars) {
             System.out.println(car);
         }
 
-        System.out.println(" ");
-        for (int i = 0; i < cars.size(); i++) {
-            System.out.println(cars.get(i));
-        }
+
 
     }
 
