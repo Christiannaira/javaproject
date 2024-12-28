067e7dfcfd6d5eb9c7838e9100173d2133cb9aa9
Christian <Christianmnaira@gmail.com>
2024-12-28 17:42:13 +0800

    feat: listIterator()

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.ListIterator;
 
 public class Main {
 
@@ -11,13 +12,21 @@ public class Main {
         ArrayList<String> cars = new ArrayList<String>();
 
         cars.add("Volvo");
-        cars.add("Ford");
         cars.add("BMW");
         cars.add("Ford");
         cars.add("Mazda");
 
-        System.out.println(cars.indexOf("Ford"));
-        System.out.println(cars.lastIndexOf("Ford"));
+        ListIterator<String> it = cars.listIterator();
+
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+
+        System.out.println("---");
+
+        while (it.hasPrevious()) {
+            System.out.println(it.previous());
+        }
 
     }
 
