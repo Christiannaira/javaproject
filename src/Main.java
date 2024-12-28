ec45bba443d929927afcfacb1d51b668061bc5a7
Christian <Christianmnaira@gmail.com>
2024-12-28 17:51:44 +0800

    feat: retainAll()

--- src/Main.java
+++ src/Main.java
@@ -9,15 +9,21 @@ public class Main {
         //familiarizing myself with Java Lists
         //ArrayList can be modified without creating a new one or copy
 
-        ArrayList<Integer> numbers = new ArrayList<Integer>();
-        numbers.add(5);
-        numbers.add(9);
-        numbers.add(8);
-        numbers.add(6);
-        numbers.add(1);
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        cars.add("Ford");
 
-        numbers.replaceAll(n -> n + 1);
-        System.out.println(numbers);
+        ArrayList<String> valid = new ArrayList<String>();
+        valid.add("Volvo");
+        valid.add("Ford");
+        valid.add("Mazda");
+
+        cars.retainAll(valid);
+
+        System.out.println(cars);
 
     }
 
