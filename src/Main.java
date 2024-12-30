ba2d63e7122c41786275141fa41dc7b4095b3da6
Christian <Christianmnaira@gmail.com>
2024-12-30 09:24:39 +0800

    feat: Arrays.fill()

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String[] cars = {"Volvo", "BMW", "Tesla", "Ford", "Fiat", "Mazda", "Audi"};
-        Arrays.sort(cars);
+        String[] fruits = {"Banana", "Orange", "Apple", "Mango"};
+        Arrays.fill(fruits, "Kiwi");
 
-        for (String i : cars) {
+        for (String i : fruits) {
             System.out.println(i);
         }
 
