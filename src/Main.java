3b3958c5ae596ec39963a30d94d37092556ff519
Christian <Christianmnaira@gmail.com>
2024-12-30 09:25:08 +0800

    feat: Arrays.fill()

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         String[] fruits = {"Banana", "Orange", "Apple", "Mango"};
-        Arrays.fill(fruits, "Kiwi");
+        Arrays.fill(fruits, 2, 4, "Kiwi");
 
         for (String i : fruits) {
             System.out.println(i);
