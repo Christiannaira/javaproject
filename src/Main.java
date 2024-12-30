234d8c05b614778ac0ba529617bb4b4b738d15b0
Christian <Christianmnaira@gmail.com>
2024-12-30 09:25:48 +0800

    feat: Arrays.length

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,8 @@ public class Main {
     public static void main(String[] args) {
 
         String[] fruits = {"Banana", "Orange", "Apple", "Mango"};
-        Arrays.fill(fruits, 2, 4, "Kiwi");
 
-        for (String i : fruits) {
-            System.out.println(i);
-        }
+        System.out.println(fruits.length);
 
         
 
