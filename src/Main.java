fe3e5dc8364434a633cf3ccde34697efb3588e20
Christian <Christianmnaira@gmail.com>
2025-01-07 10:35:57 +0800

    feat: using multiple classes

--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,7 @@ public class Main {
         System.out.println("Max speed is: " + maxSpeed);
     }
 
-     public static void main(String[] args) {
 
-        Main myCar = new Main();
-        myCar.fullThrottle();
-        myCar.speed(200);
-
-     }
 
 
 }
--- src/Second.java
+++ src/Second.java
@@ -1,4 +1,12 @@
 public class Second {
 
+    public static void main(String[] args) {
+
+        Main myCar = new Main();
+        myCar.fullThrottle();
+        myCar.speed(200);
+
+
+    }
 
 }
