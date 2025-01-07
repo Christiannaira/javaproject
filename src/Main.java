21d129dd337bd06ff8034d9489e73530c6ba434f
Christian <Christianmnaira@gmail.com>
2025-01-07 10:22:34 +0800

    feat: modifying a class attribute

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 
 public class Main {
 
-    int x = 5;
+    int x;
 
     public static void main(String[] args) {
 
         Main myObj = new Main();
-        myObj.x = 40;
+        myObj.x = 25;
         System.out.println(myObj.x);
 
     }
