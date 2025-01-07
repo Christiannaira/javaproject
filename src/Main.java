efa50180f006f796e877a2a47a6e489161520b16
Christian <Christianmnaira@gmail.com>
2025-01-07 10:22:54 +0800

    feat: empty class attribute

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         Main myObj = new Main();
+        System.out.println(myObj.x);
         myObj.x = 25;
         System.out.println(myObj.x);
 
