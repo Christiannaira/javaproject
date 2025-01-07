6b5baee3b5329e46dd970546390f731c6fd5c408
Christian <Christianmnaira@gmail.com>
2025-01-07 10:21:43 +0800

    feat: accessing a class attribute

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,12 @@ public class Main {
 
     int x = 5;
 
+    public static void main(String[] args) {
 
+        Main myObj = new Main();
+        System.out.println(myObj.x);
+
+    }
 
 
 
--- src/Second.java
+++ src/Second.java
@@ -1,8 +1,4 @@
 public class Second {
 
-    public static void main(String[] args) {
-        Main myObj = new Main();
-        System.out.println(myObj.x);
-    }
 
 }
