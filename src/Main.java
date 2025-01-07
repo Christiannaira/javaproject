fbe746c09194baaa5e1cfd8e7530cfbf0ac293e7
Christian <Christianmnaira@gmail.com>
2025-01-07 10:24:40 +0800

    feat: multiple objects from class

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
 
 public class Main {
 
-    final int x = 10;
+    int x = 5;
 
     public static void main(String[] args) {
 
-        Main myObj = new Main();
-        myObj.x = 25;
-        System.out.println(myObj.x);
+        Main myObj1 = new Main();
+        Main myObj2 = new Main();
+        myObj1.x = 25;
+        System.out.println(myObj1.x);
+        System.out.println(myObj1.x);
 
     }
 
