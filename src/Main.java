a0abdf98232d6336ec95c134d53afaadcdb48c48
Christian <Christianmnaira@gmail.com>
2025-01-27 16:17:50 +0800

    feat: checked Arrays.equals(val1, val2);

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
-    public void fullThrottle() {
-        System.out.println("The car is going as fast as it can!");
+    public static void main(String[] args) {
+
+        String[] numbers = {"1", "2", "3", "4", "5"};
+        Integer[] numbers02 = {1, 2, 3, 4, 5};
+        String[] numbers03 = {"1", "2", "3", "4", "6"};
+
+
+//        Scanner askUser = new Scanner(System.in);
+//        System.out.print("Enter your number: ");
+//        String userInput = askUser.nextLine();
+
+        if (Arrays.equals(numbers, numbers03)) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+
     }
-
-    public void speed(int maxSpeed) {
-        System.out.println("Max speed is: " + maxSpeed);
-    }
-
-
-
 
 }
--- src/Second.java
+++ src/Second.java
@@ -2,9 +2,7 @@ public class Second {
 
     public static void main(String[] args) {
 
-        Main myCar = new Main();
-        myCar.fullThrottle();
-        myCar.speed(200);
+
 
 
     }
