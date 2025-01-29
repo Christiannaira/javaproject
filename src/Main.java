5ca3c6b3e035776c3f8357311596c2dca66a0fc6
Christian <Christianmnaira@gmail.com>
2025-01-29 17:58:30 +0800

    feat: class method static vs public

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,20 @@ import java.util.Scanner;
 
 public class Main {
 
-    static void myMethod() {
-        System.out.println("Hello World!");
+    static void myStaticMethod(){
+        System.out.println("Static methods can be called without creating objects");
+    }
+
+    public void myPublicMethod() {
+        System.out.println("Public methods must be called by creating objects");
     }
 
     public static void main(String[] args) {
 
-        myMethod();
+        myStaticMethod();
+
+        Main myObj = new Main();
+        myObj.myPublicMethod();
 
     }
 
