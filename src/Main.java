f82750006bc575ad9feaeb65b0b6404700cdc204
Christian <Christianmnaira@gmail.com>
2025-01-29 17:40:22 +0800

    feat: creating an object

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,12 @@ import java.util.Scanner;
 
 public class Main {
 
+    int x = 5;
+
     public static void main(String[] args) {
 
-        String[] numbers = {"1", "2", "3", "4", "5"};
-        Integer[] numbers02 = {1, 2, 3, 4, 5};
-        String[] numbers03 = {"1", "2", "3", "4", "6"};
-
-
-//        Scanner askUser = new Scanner(System.in);
-//        System.out.print("Enter your number: ");
-//        String userInput = askUser.nextLine();
-
-        if (Arrays.equals(numbers, numbers03)) {
-            System.out.println(true);
-        } else {
-            System.out.println(false);
-        }
+        Main myObj = new Main();
+        System.out.println(myObj.x);
 
     }
 
