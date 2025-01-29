d4d9606c21abf7048924968f86fb1d32ca95ccce
Christian <Christianmnaira@gmail.com>
2025-01-29 17:45:51 +0800

    feat: final keyword to class attribute so it can't be modified

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,11 @@ import java.util.Scanner;
 
 public class Main {
 
-    int x = 10;
+    final int x = 10;
 
     public static void main(String[] args) {
 
       Main myObj = new Main();
-      myObj.x = 25;
       System.out.println(myObj.x);
 
     }
