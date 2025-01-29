58013ab1364a097a1cca76a51746ce8ada31ec0d
Christian <Christianmnaira@gmail.com>
2025-01-29 17:47:26 +0800

    feat: multiple objects

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,17 @@ import java.util.Scanner;
 
 public class Main {
 
-    final int x = 10;
+    int x = 5;
 
     public static void main(String[] args) {
 
-      Main myObj = new Main();
-      System.out.println(myObj.x);
+      Main myObj1 = new Main();
+      Main myObj2 = new Main();
+
+      myObj2.x = 25;
+
+      System.out.println(myObj1.x);
+      System.out.println(myObj2.x);
 
     }
 
