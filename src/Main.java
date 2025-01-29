7e50d91911a1972921d8f25706182255b99d04d5
Christian <Christianmnaira@gmail.com>
2025-01-29 19:38:35 +0800

    feat: constructor parameter

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,13 @@ public class Main {
     int x;
 
     // Create a class constructor for the Main class
-    public Main() {
-        x = 5;
+    public Main(int y) {
+        x = y;
     }
 
     public static void main(String[] args) {
 
-        Main myObj = new Main();
+        Main myObj = new Main(5);
         System.out.println(myObj.x);
 
     }
