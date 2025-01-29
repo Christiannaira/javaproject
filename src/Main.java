dcf3084c7cb6007d02d023c42331e7bf66cb34e1
Christian <Christianmnaira@gmail.com>
2025-01-29 19:42:20 +0800

    feat: constructor parameters

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,19 @@ import java.util.Scanner;
 
 public class Main {
 
-    int x;
+    int modelYear;
+    String modelName;
 
     // Create a class constructor for the Main class
-    public Main(int y) {
-        x = y;
+    public Main(int year, String name) {
+        this.modelYear = year;
+        this.modelName = name;
     }
 
     public static void main(String[] args) {
 
-        Main myObj = new Main(5);
-        System.out.println(myObj.x);
+        Main myCar = new Main(1969, "Mustang");
+        System.out.println(myCar.modelYear + " " + myCar.modelName);
 
     }
 
