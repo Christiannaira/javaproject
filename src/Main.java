aba94ae5e0eea1bace7fc62a54a74102043afc38
Christian <Christianmnaira@gmail.com>
2025-01-29 19:46:54 +0800

    feat: using final non-access modifier

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,14 @@ import java.util.Scanner;
 
 public class Main {
 
-    int modelYear;
-    String modelName;
+    final int x = 10;
+    final double PI = 3.14;
 
-    // Create a class constructor for the Main class
-    public Main(int year, String name) {
-        this.modelYear = year;
-        this.modelName = name;
-    }
 
     public static void main(String[] args) {
 
-        Main myCar = new Main(1969, "Mustang");
-        System.out.println(myCar.modelYear + " " + myCar.modelName);
+        Main myObj = new Main();
+        System.out.println(myObj.x);
 
     }
 
