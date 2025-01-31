0560518d18c2c31c9e0a94d36cafdd03c4b4fc9a
Christian <Christianmnaira@gmail.com>
2025-01-31 21:07:28 +0800

    feat: java inheritance

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,6 @@
-import technical.Prog;
 
 public abstract class Main {
 
-    public static void main(String[] args) {
 
-      Prog firstName = new Prog();
-
-        System.out.println(firstName.fname);
-        firstName.playSound();
-
-    }
 
 }
