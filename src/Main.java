e3d913e509d332197caf01dbcbcbaa320a0bf6bc
Christian <Christianmnaira@gmail.com>
2025-02-04 21:41:13 +0800

    feat: private access modifier

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,21 @@
 
 class Main {
 
+    private String fname = "John";
+    private String lname = "Doe";
+    private String email = "john@doe.com";
+    private int age = 24;
+
     public static void main(String[] args) {
 
-        NewClass anotherClass = new NewClass();
+        Main myObj = new Main();
 
-        System.out.println(anotherClass.printResult());
-
-        System.out.println(newMethod());
-
-    }
-
-    public static String newMethod(){
-
-        return "new method returned";
+        System.out.println("Name: " + myObj.fname + " " + myObj.lname);
+        System.out.println("Email: " + myObj.email);
+        System.out.println("Age: " + myObj.age);
 
     }
+
+
 
 }
