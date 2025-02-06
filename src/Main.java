e9908fc0c22b0bffe9d294326eb27ae1626c6042
Christian <Christianmnaira@gmail.com>
2025-02-07 01:40:01 +0800

    feat: java.lang.Character isLowerCase

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
 
+import java.lang.Character;
+
 class Main {
 
 
     public static void main(String[] args) {
 
-        System.out.println("hello world");
+        char firstLetter = 'a';
+        char secondLetter = 'A';
+
+        System.out.println(Character.isLowerCase(firstLetter));
+        System.out.println(Character.isLowerCase(secondLetter));
 
     }
 
