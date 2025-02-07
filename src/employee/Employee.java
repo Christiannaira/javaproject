1d7c0162b2b8aea84bb98f188187bdda2493b753
Christian <Christianmnaira@gmail.com>
2025-02-07 16:10:43 +0800

    feat: class inheritance implementation

--- src/employee/Employee.java
+++ src/employee/Employee.java
@@ -1,5 +1,12 @@
 package employee;
 
+// Make a class Employee with a name and salary. Make a class Manager inherit from Employee.
+// Add an instance variable, named department, of type String.
+// Supply a method toString that prints that manager's name, department, and salary.
+// Make a class Executive inherit from Manager.
+// Supply appropriate toString methods for all classes.
+// Supply a test program that tests the classes and methods.
+
 public class Employee {
 
     String name;
