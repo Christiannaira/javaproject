fc7b2af811d79eb490a2daf431c6c6fab6bcba27
Christian <Christianmnaira@gmail.com>
2025-02-07 20:43:32 +0800

    feat: method overriding

---
+++ src/inheritance/Naira.java
@@ -0,0 +1,54 @@
+package inheritance;
+
+// use of superclass
+public class Naira {
+
+    String skill;
+
+    public Naira(String skill){
+
+        this.skill = skill;
+
+    }
+
+    public String toString(){
+
+        return "My skill is " + skill;
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+            Naira surname = new Naira("coding");
+            Christian firstName = new Christian("coding","singing", "taken");
+
+        System.out.println(surname);
+        System.out.println(firstName);
+
+    }
+
+
+}
+
+// use of subclass
+class Christian extends Naira {
+
+    String talent;
+    String status;
+
+    public Christian(String skill, String talent, String status){
+        super(skill);
+        this.talent = talent;
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+
+        return "My skill is " + skill + " \n" + "My talent is " + talent + "\n" + "My status is " + status;
+
+    }
+
+}
