4af3f1f9f24f961b4abaa194a4918c1648027f53
Christian <Christianmnaira@gmail.com>
2025-02-08 08:54:27 +0800

    feat: loop through an enumeration

--- src/Main.java
+++ src/Main.java
@@ -11,19 +11,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Level myVar = Level.HIGH;
 
-        switch (myVar) {
+        for (Level myVar : Level.values()) {
 
-            case LOW:
-                System.out.println("Low level");
-                break;
-            case MEDIUM:
-                System.out.println("Medium level");
-                break;
-            case HIGH:
-                System.out.println("High level");
-                break;
+            System.out.println(myVar);
 
         }
 
