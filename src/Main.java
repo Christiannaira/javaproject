759a4f2286ad9c7274875856ff8333584207d21b
Christian <Christianmnaira@gmail.com>
2025-02-08 08:52:47 +0800

    feat: enumeration inside switch

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,20 @@ public class Main {
     public static void main(String[] args) {
 
         Level myVar = Level.HIGH;
-        System.out.println(myVar);
+
+        switch (myVar) {
+
+            case LOW:
+                System.out.println("Low level");
+                break;
+            case MEDIUM:
+                System.out.println("Medium level");
+                break;
+            case HIGH:
+                System.out.println("High level");
+                break;
+
+        }
 
     }
 
