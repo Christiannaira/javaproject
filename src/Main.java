4875ed0e9927b8095b0dd25589acd6c6ee4bab10
Christian <Christianmnaira@gmail.com>
2025-02-09 19:50:46 +0800

    feat: file handling (delete a file)

--- src/Main.java
+++ src/Main.java
@@ -11,17 +11,12 @@ public class Main {
 
         File myObj = new File("filename.txt");
 
-        if (myObj.exists()) {
-            System.out.println("File name: " + myObj.getName());
-            System.out.println("Absolute path: " + myObj.getAbsolutePath());
-            System.out.println("Writeable: " + myObj.canWrite());
-            System.out.println("Readable: " + myObj.canRead());
-            System.out.println("File size in bytes: " + myObj.length());
+        if (myObj.delete()) {
+            System.out.println("Deleted the file: " + myObj.getName());
         } else {
-
-            System.out.println("The file does not exist.");
-
+            System.out.println("Failed to delete the file");
         }
+
 
     }
 
