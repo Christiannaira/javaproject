8f37987e77d7ae5b5cd7f15325d758a4030b2994
Christian <Christianmnaira@gmail.com>
2025-02-09 19:40:54 +0800

    feat: file handling (write to a file) -> override

--- src/Main.java
+++ src/Main.java
@@ -10,10 +10,11 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-            FileWriter myWriter = new FileWriter("filename.txt");
-            myWriter.write("Files in Java might be tricky, but it is fun enough!");
-            myWriter.close();
-            System.out.println("Successfully wrote to the file");
+
+            FileWriter myObj = new FileWriter("filename.txt");
+            myObj.write("Hello World");
+            myObj.close();
+            System.out.println("Successfully wrote to the file!");
 
         } catch (IOException e) {
 
