97c8378d0eb2d619ab2161e4215b36e0fb419c1b
Christian <Christianmnaira@gmail.com>
2025-02-09 19:13:15 +0800

    feat: file handling (creating a file) -> added label comments

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-            File myObj = new File("filename.txt");
+            File myObj = new File("");
             if (myObj.createNewFile()) {
 
                 System.out.println("File created: " + myObj.getName());
@@ -24,7 +24,11 @@ public class Main {
 
             System.out.println("An error occured.");
 
+            e.printStackTrace(); // Detailed stack trace
+
         }
+
+        // Execution continue even after the exception
 
 
     }
