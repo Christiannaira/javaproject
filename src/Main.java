d99c5cab30e3f02836062aeddf62c85d0df57a3a
Christian <Christianmnaira@gmail.com>
2025-02-14 21:43:46 +0800

    feat: java break

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        for (int i = 0; i < 10; i++) {
-            if (i == 4) {
-                continue;
-            }
+        int i = 0;
+
+        while (i < 10) {
 
             System.out.println(i);
+            i++;
+            if (i == 4) {
+                break;
+            }
+
         }
 
     }
