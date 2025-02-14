382393a5c82dc1b5402da873f940de658db17c76
Christian <Christianmnaira@gmail.com>
2025-02-14 21:30:41 +0800

    feat: narrowing casting

--- src/Main.java
+++ src/Main.java
@@ -6,20 +6,11 @@ public class Main {
     public static void main(String[] args) {
 
 
-        byte myByte = 1;
-        short myShort = myByte;
-        int myInt = myShort;
-        long myLong = myInt;
-        float myFloat = myLong;
-        double myDouble = myFloat;
+        double myDouble = 9.78d;
+        int myInt = (int) myDouble;
 
-        System.out.println(myByte);
-        System.out.println(myShort);
-        System.out.println(myInt);
-        System.out.println(myLong);
-        System.out.println(myFloat);
         System.out.println(myDouble);
-
+        System.out.println(myInt);
 
     }
 
