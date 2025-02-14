fe28ae94fc59bd88296b022778ae2715bbf7d350
Christian <Christianmnaira@gmail.com>
2025-02-14 22:28:56 +0800

    feat: Palindrome challenge

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,41 @@
-
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        char myGrade = 'B';
-        System.out.println(myGrade);
+       // Palindrome
+        Scanner askUser = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+
+        String num = askUser.nextLine();
+
+        boolean palindrome = isPalindrome(num);
+
+        if (palindrome) {
+            System.out.println(num + " is a Palindrome");
+        } else {
+            System.out.println(num + " is not a Palindrome");
+        }
+
+    }
+
+    public static boolean isPalindrome(String num) {
+
+        String originalNumber = num;
+        String reversedNumber = "";
+
+        for (int i = num.length() - 1; i >= 0; i--) {
+            reversedNumber += num.charAt(i);
+        }
+
+        if (originalNumber.equals(reversedNumber)) {
+            return true;
+        } else {
+            return false;
+        }
+
 
 
     }
