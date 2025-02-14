e5bf7d172db4cba1697833e5d988e7f3efb1dc6f
Christian <Christianmnaira@gmail.com>
2025-02-14 21:34:19 +0800

    feat: logical ! operator

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         int x = 5;
-        System.out.println(x > 3 || x < 4);
+        System.out.println(!(x > 3 || x < 4));
 
     }
 
