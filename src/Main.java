f966bad0d6f871e2c28dbf0df5fbbe9547edceff
Christian <Christianmnaira@gmail.com>
2025-02-14 22:07:52 +0800

    feat: char data type

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,12 @@
-import javax.swing.*;
-import java.awt.*;
-import java.lang.reflect.Array;
+
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int ages[] = {20, 22, 18, 35, 48, 26, 87, 70};
+        char myGrade = 'B';
+        System.out.println(myGrade);
 
-        float avg, sum = 0;
-
-        int length = ages.length;
-
-        for (int age : ages) {
-
-            sum += age;
-
-        }
-
-        avg = sum / length;
-
-        System.out.println("The average age is: " + avg);
 
     }
 
