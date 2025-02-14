22f39bbf5376a337ad1c4a0c0d4721fe69db3a56
Christian <Christianmnaira@gmail.com>
2025-02-14 21:39:24 +0800

    feat: comparison == operator

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,9 @@ public class Main {
     public static void main(String[] args) {
 
         int x = 5;
-        System.out.println(!(x > 3 || x < 4));
+        int y = 3;
+
+        System.out.println(x == y);
 
     }
 
