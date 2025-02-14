5fdc87b24cfd737bc4fea3507d9f1098b735e981
Christian <Christianmnaira@gmail.com>
2025-02-14 21:54:18 +0800

    feat: loop through an array

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,16 @@
 import javax.swing.*;
 import java.awt.*;
+import java.lang.reflect.Array;
 
 public class Main {
 
     public static void main(String[] args) {
 
         String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
-        int[] myNum = {10, 20, 30, 40};
 
-        System.out.println(cars[0]);
-        System.out.println(cars[1]);
-        System.out.println(cars[2]);
-        System.out.println(cars[3]);
-
-        myNum[0] = 20;
-
-        System.out.println(myNum[0]);
-        System.out.println(myNum[1]);
-        System.out.println(myNum[2]);
-        System.out.println(myNum[3]);
-
-        System.out.println(cars.length);
-
-
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println(cars[i]);
+        }
 
     }
 
