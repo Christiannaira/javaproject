85291e4b283f63d03643db45ff3073718bf25d88
Christian <Christianmnaira@gmail.com>
2025-02-14 21:29:03 +0800

    feat: data types conversion

--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,22 @@ import java.awt.*;
 
 public class Main {
 
-
     public static void main(String[] args) {
 
-        // JFrame = a GUI window to add components to
 
-//        JFrame frame = new JFrame(); // creates a frame
-//        frame.setVisible(true); // make frame visible
-//        frame.setSize(500, 500); // sets the x-dimension and y-dimension of frame
-//        frame.setTitle("My First JAVA GUI Program"); // sets title of frame
-//        frame.setResizable(false); // make frame visible
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit out of application
-//
-//        ImageIcon image = new ImageIcon("src\\dailydev.png"); // creates an ImageIcon
-//        frame.setIconImage(image.getImage()); // change the icon of frame
-//
-//        frame.getContentPane().setBackground(Color.green);
-//        frame.getContentPane().setBackground(new Color(0,0,0)); // change color of background
-//        frame.getContentPane().setBackground(new Color(0x123456)); // change color of background
+        byte myByte = 1;
+        short myShort = myByte;
+        int myInt = myShort;
+        long myLong = myInt;
+        float myFloat = myLong;
+        double myDouble = myFloat;
 
-        MyFrame frame = new MyFrame();
+        System.out.println(myByte);
+        System.out.println(myShort);
+        System.out.println(myInt);
+        System.out.println(myLong);
+        System.out.println(myFloat);
+        System.out.println(myDouble);
 
 
     }
